d86d16056ff53ccc0a3c8cdcff6789080ae04009
katamzyshahin <katamzy.shahin@gmail.com>
2026-07-27 23:18:23 +0200

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addCollection("portfolioZuhause", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/portfolio/*.md")
+      .filter((item) => item.data.category === "Zuhause")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  eleventyConfig.addCollection("portfolioWerkstatt", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/portfolio/*.md")
+      .filter((item) => item.data.category === "Werkstatt")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  eleventyConfig.addCollection("testimonials", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/testimonials/*.md")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  eleventyConfig.addCollection("products", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/products/*.md")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
